0fb6b2532b833f279dda1669850b66fc70f8995f
Sirius <h_star@foxmail.com>
2026-04-05 14:48:32 +0800

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+        int[] freq = new int[26];
+
+        for(int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a'] ++;
+        }
+
+        for(int i = 0; i < t.length(); i++) {
+            int num = freq[t.charAt(i) - 'a'];
+
+            if (num == 0) return false;
+
+            freq[t.charAt(i) - 'a']--;
+        }
+
+        return true;
+    }
+}
